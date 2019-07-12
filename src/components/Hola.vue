6582e4b6ad4a808e31aed2e4790d4cb8cf0d4806
jeffvilla <csjeffri1994@gmail.com>
2019-07-12 21:09:19 +0200

    Primer Commit
    Trabajo de Vue Pruebas

---
+++ src/components/Hola.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <H1>Hola : {{status }}</H1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Hola",
+  data() {
+    return {
+      status: "Mal"
+    };
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Hola></Hola>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Hola from "@/components/Hola.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    Hola
+  }
+};
+</script>
